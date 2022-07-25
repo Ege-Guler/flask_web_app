8ec3d13b0817f44779518d4e877af923b4b0e12c
Ege <egeguler2003@gmail.com>
2022-07-25 22:38:39 +0300

    minor fixes

--- page.py
+++ page.py
@@ -1,11 +1,16 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 
 
 app = Flask(__name__)
 
+
+app.config['SECRET_KEY'] = 'a1ba82a9c0673d28b00c9520f4963a95'
+
+
 @app.route("/", methods = ["POST", "GET"])
 def home():
+	
 	if request.method == "POST":
 
 		row1 = request.form.get("row1")
@@ -14,11 +19,16 @@ def home():
 		col2 = request.form.get("col2")
 
 		if row1 == col1:
+
 			return redirect(url_for("multiplication", r1 = row1, c1 = col1, r2 = row2, c2 = col2))
 
 
-	else:	
-		return render_template("home.html")
+		else:
+			flash(f'asda', 'error')
+			return render_template('home.html')
+
+
+	return render_template('home.html')
 
 @app.route("/multiplication/<r1>/<c1>/<r2>/<c2>/")
 def multiplication(r1, c1, r2, c2):
